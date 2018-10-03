af869092a58c8395b498fd816580f52f31b0ece6
Marcos Stark <mrcs.stark@gmail.com>
2018-10-03 02:26:37 -0300

    Imeplementa roteamento

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -16,4 +16,9 @@ export class HeroService {
     this.messageservice.add('HeroService: Heróis buscados');
     return of(HEROES);  
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageservice.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));  
+  }
 }
